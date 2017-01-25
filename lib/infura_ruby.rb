0be09d1434ec4c707a1860ee3d2768a5f16b5571
jborrey <julianborrey@gmail.com>
2017-01-24 21:58:32 -0800

    fix typo on keyword arg

--- lib/infura_ruby.rb
+++ lib/infura_ruby.rb
@@ -7,7 +7,7 @@ module InfuraRuby
 
   class << self
     # Generate a new client for the Infura api.
-    def client(api_key: api_key, network: :main)
+    def client(api_key:, network: :main)
       Client.new(api_key: api_key, network: network)
     end
   end
