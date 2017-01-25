f2a0d90792624a8849bbbbe327a82b9338e5a335
jborrey <julianborrey@gmail.com>
2017-01-24 21:56:43 -0800

    add custom errors

--- lib/infura_ruby.rb
+++ lib/infura_ruby.rb
@@ -2,6 +2,9 @@ require 'infura_ruby/version'
 require 'infura_ruby/client'
 
 module InfuraRuby
+  class InvalidApiKeyError < StandardError; end
+  class InvalidNetworkError < StandardError; end
+
   class << self
     # Generate a new client for the Infura api.
     def client(api_key: api_key, network: :main)
